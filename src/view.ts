fb047a8c37be99cc0623efa6d0606f28d0137301
Eric Amodio <eamodio@gmail.com>
2021-07-13 01:58:30 -0400

    Adds v11.6.0 & v11.5.1

--- src/view.ts
+++ src/view.ts
@@ -6,7 +6,7 @@ export class View {
 	private classes: string[];
 	private observer: IntersectionObserver | undefined;
 
-	private activeVersion: string | undefined = '11.5.0';
+	private activeVersion: string | undefined = '11.6.0';
 	private versions = new Map<string, boolean>();
 
 	constructor(public name: string) {
