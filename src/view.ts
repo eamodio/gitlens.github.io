2e4e0a6aa821f159a65a18847414c76dfc984163
Eric Amodio <eamodio@gmail.com>
2022-02-24 03:53:17 -0500

    Adds v12

--- src/view.ts
+++ src/view.ts
@@ -6,7 +6,7 @@ export class View {
 	private classes: string[];
 	private observer: IntersectionObserver | undefined;
 
-	private activeVersion: string | undefined = '11.7.0';
+	private activeVersion: string | undefined = '12.0.0';
 	private versions = new Map<string, boolean>();
 
 	constructor(public name: string) {
